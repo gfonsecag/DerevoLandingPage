2f694b986bd861162b621ac0c1ef7370d4cca3f9
BinOverlord <casillaschavezjuanpedro@gmail.com>
2017-04-25 09:41:12 -0500

    250420170940

--- js/init.js
+++ js/init.js
@@ -1,3 +1,39 @@
 
   // Initialize collapse button
+
+ $(document).ready(function(){
+    $('.carousel').carousel({});
+//    $('.scrollspy').scrollSpy();
+
+//  var options = [ 
+//    {selector: '#fact-row', offset: 50, callback: function(el){
+//       console.log("Fire fire fire fire");
+
+//       $('.count').each(function () {
+//         $(this).prop('Counter',0).animate({
+//             Counter: $(this).text()
+//         }, {
+//             duration: 10000,
+//             easing: 'swing',
+//             step: function (now) {
+//                 $(this).text('+' + Math.ceil(now));
+//             }
+//         });
+//     });
+//   } } ]; 
+//   console.log("Fire fire");
+  // Materialize.scrollFire(options); 
+
+  });
+
+  var options = [ {
+    selector: '#fact-row', offset: 50, callback: function(el) { 
+      Materialize.toast("This is our ScrollFire Demo!", 1500 ); } 
+    }, {selector: '#fact-row', offset: 205, callback: function(el) { 
+      Materialize.toast("Please continue scrolling!", 1500 ); } 
+    } ];
+       
+    Materialize.scrollFire(options); 
+
+
   $(".button-collapse").sideNav();
