c2d8a0bad260f694e5453e8711f78b3b20b4f8ee
BinOverlord <casillaschavezjuanpedro@gmail.com>
2017-05-05 16:32:25 -0500

    050520150432

--- js/init.js
+++ js/init.js
@@ -1,42 +1,78 @@
-  $(".button-collapse").sideNav();
+$(".button-collapse").sideNav();
 
+var options = [{
+  selector: '.count', offset: 0, callback: function (el) {
+    Materialize.toast("This is our ScrollFire Demo!", 1500);
+  }
+}, {
+  selector: '#fact-row', offset: 0, callback: function (el) {
+    Materialize.toast("Please continue scrolling!", 1500);
+  }
+}];
 
-  var options = [ {
-    selector: '.count', offset: 0, callback: function(el) { 
-      Materialize.toast("This is our ScrollFire Demo!", 1500 ); } 
-    }, {selector: '#fact-row', offset: 0, callback: function(el) { 
-      Materialize.toast("Please continue scrolling!", 1500 ); } 
-    } ];
-       
-    Materialize.scrollFire(options); 
+Materialize.scrollFire(options);
 
- $(document).ready(function(){
-    $('.carousel.carousel-logo').carousel({dist:-30,shift: 0, padding: 65});
-    $('.carousel.carousel-slider').carousel({fullWidth: true});
-    $('.modal').modal();
+$(document).ready(function () {
 
-//    $('.scrollspy').scrollSpy();
+  $('.text-carousel').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    asNavFor: '.icon-carousel'
+  });
+  $('.icon-carousel').slick({
+    centerMode: true,
+    centerPadding: '100px',
+    slidesToShow: 5,
+    asNavFor: '.text-carousel',
+    adaptiveHeight: true,
+    focusOnSelect: true,
+    responsive: [
+      {
+        breakpoint: 780,
+        settings: {
+          centerMode: true,
+          centerPadding: '0px',
+          slidesToShow: 5,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '0px',
+          slidesToShow: 3,
+          arrows: false,
+        }
+      }
+    ]
+  });
 
-//  var options = [ 
-//    {selector: '#fact-row', offset: 50, callback: function(el){
-//       console.log("Fire fire fire fire");
+  $('.modal').modal();
 
-//       $('.count').each(function () {
-//         $(this).prop('Counter',0).animate({
-//             Counter: $(this).text()
-//         }, {
-//             duration: 10000,
-//             easing: 'swing',
-//             step: function (now) {
-//                 $(this).text('+' + Math.ceil(now));
-//             }
-//         });
-//     });
-//   } } ]; 
-//   console.log("Fire fire");
+  //    $('.scrollspy').scrollSpy();
+
+  //  var options = [ 
+  //    {selector: '#fact-row', offset: 50, callback: function(el){
+  //       console.log("Fire fire fire fire");
+
+  //       $('.count').each(function () {
+  //         $(this).prop('Counter',0).animate({
+  //             Counter: $(this).text()
+  //         }, {
+  //             duration: 10000,
+  //             easing: 'swing',
+  //             step: function (now) {
+  //                 $(this).text('+' + Math.ceil(now));
+  //             }
+  //         });
+  //     });
+  //   } } ]; 
+  //   console.log("Fire fire");
   // Materialize.scrollFire(options); 
 
-  });
+});
 
 
 
