fc6b997730f7a0e15a552b0e67fe245f3410f2e4
BinOverlord <casillaschavezjuanpedro@gmail.com>
2017-05-08 17:33:17 -0500

    080520170532

--- js/init.js
+++ js/init.js
@@ -75,5 +75,12 @@ $(document).ready(function () {
 });
 
 
+var figure = $("#we2").hover( hoverVideo, hideVideo );
 
-// Tv off 
+function hoverVideo(e) {  
+    $('video', this).get(0).play(); 
+}
+
+function hideVideo(e) {
+    $('video', this).get(0).pause();
+}
